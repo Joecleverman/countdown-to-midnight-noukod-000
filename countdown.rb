e69509e7e2a2a4c299fe67df819e2490c9f7860b
Josue Abraham <josuea@noukod.com>
2018-09-14 19:18:47 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,12 +3,12 @@
 def countdown(number)
   number = 10
 while number > 0
-   sleep(0.5)
+   #sleep(0.5)
 puts "#{number} SECOND(S)!"
 number -= 1
 end
 return "HAPPY NEW YEAR!"
 end
-#def countdown_with_sleep(runtime)
-#sleep(1.seconds)
-#end
+def countdown_with_sleep(runtime)
+sleep(0.5)
+end
